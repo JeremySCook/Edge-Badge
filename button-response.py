932c0d92e87c18e009ad730edd73fd86d487f87b
Jeremy S Cook <jscook55@gmail.com>
2020-01-23 16:15:51 -0500

    Create button-response.py

---
+++ button-response.py
@@ -0,0 +1,32 @@
+# CircuitPython IO demo #1 - General Purpose I/O
+# When badge button a pressed, pin 13 goes high.
+# Need to add LED separately
+
+from adafruit_pybadger import PyBadger
+
+pybadger = PyBadger()
+
+import time
+import board
+from digitalio import DigitalInOut, Direction, Pull
+
+led = DigitalInOut(board.D13)
+led.direction = Direction.OUTPUT
+
+# For Gemma M0, Trinket M0, Metro M0 Express, ItsyBitsy M0 Express, Itsy M4 Express
+switch = DigitalInOut(board.D2)
+# switch = DigitalInOut(board.D5)  # For Feather M0 Express, Feather M4 Express
+# switch = DigitalInOut(board.D7)  # For Circuit Playground Express
+switch.direction = Direction.INPUT
+switch.pull = Pull.UP
+
+first_display = True
+
+while True:
+    # We could also do "led.value = not switch.value"!
+    if pybadger.button.a:
+        led.value = True
+    else:
+        led.value = False
+
+    time.sleep(0.01)  # debounce delay
